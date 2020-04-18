7b393a901cb011e2fb3b6edcf0f35ad80f2b2cb2
ariellopes94 <ariel.edit@hotmail.com>
2020-04-18 16:23:26 -0400

    Cofigurando Spring Boot

---
+++ Projeto Comercial/backend comercial-api/src/test/java/com/ariellopes/comercial/ComercialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariellopes.comercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
